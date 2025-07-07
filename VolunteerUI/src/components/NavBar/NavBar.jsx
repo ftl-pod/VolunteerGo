9f7365f271840ffc48e7840905cd1b10c7be366d
jandresol <jasmine.andresol@gmail.com>
2025-07-07 15:28:55 -0700

    styling for search page + navbar

--- VolunteerUI/src/components/NavBar/NavBar.jsx
+++ VolunteerUI/src/components/NavBar/NavBar.jsx
@@ -4,34 +4,37 @@ import { useState } from 'react';
 import { FaUserCircle } from "react-icons/fa";
 
 function Navbar() {
-    const [isLoggedIn, setIsLoggedIn] = useState(true); // TEMP for example
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
     const navigate = useNavigate();
 
     const handleLogout = () => {
-        // TODO: Call your actual logout logic (e.g. clear token, reset auth context)
         setIsLoggedIn(false);
         navigate('/login');
     };
 
     return (
         <nav className="navbar">
-        <NavLink to="/" className="nav-link">Home</NavLink>
-        <NavLink to="/search" className="nav-link">Search</NavLink>
-        <NavLink to="/leaderboard" className="nav-link">Leaderboard</NavLink>
+        <div className="navbar-links">
+            <NavLink to="/" exact className="nav-link">Home</NavLink>
+            <NavLink to="/search" className="nav-link">Search</NavLink>
+            <NavLink to="/leaderboard" className="nav-link">Leaderboard</NavLink>
+        </div>
 
-        {isLoggedIn ? (
+        <div className="profile-menu">
+            {isLoggedIn ? (
             <>
-            <NavLink to="/profile" className="profile-icon">
+                <NavLink to="/profile" className="profile-icon">
                 <FaUserCircle size={20} />
-            </NavLink>
-            <button className="logout-button" onClick={handleLogout}>Logout</button>
+                </NavLink>
+                <button className="logout-button" onClick={handleLogout}>Logout</button>
             </>
-        ) : (
+            ) : (
             <>
-            <NavLink to="/login" className="nav-link">Login</NavLink>
-            <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
+                <NavLink to="/login" className="nav-link">Login</NavLink>
+                <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
             </>
-        )}
+            )}
+        </div>
         </nav>
     );
     }

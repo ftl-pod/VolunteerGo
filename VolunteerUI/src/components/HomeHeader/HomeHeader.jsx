6853fefab6280ad8ee70eb7e3bd41c55336b160c
avisink <ayomidevanessa@gmail.com>
2025-07-11 09:41:01 -0700

    CSS selector sepcificity

--- VolunteerUI/src/components/HomeHeader/HomeHeader.jsx
+++ VolunteerUI/src/components/HomeHeader/HomeHeader.jsx
@@ -6,7 +6,7 @@ function HomeHeader() {
   return (
     <div className="home-header">
       <div className="header-left">
-        <div className="logo-placeholder">
+        <div className="logo-placeholder-1">
           <FaRegPaperPlane />
         </div>
         <div className="org-name">

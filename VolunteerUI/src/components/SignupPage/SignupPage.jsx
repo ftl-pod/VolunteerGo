3ea4888c100af6decaade67200c856c3aaae2a37
jandresol <jasmine.andresol@gmail.com>
2025-07-15 14:54:06 -0700

    clerk styling

--- VolunteerUI/src/components/SignupPage/SignupPage.jsx
+++ VolunteerUI/src/components/SignupPage/SignupPage.jsx
@@ -1,70 +1,20 @@
 import "./Signup.css";
 import { useNavigate } from "react-router-dom";
+import { SignUp } from '@clerk/clerk-react';
 
 function SignupPage() {
   const navigate = useNavigate();
   return (
     <>
-        <div className="auth-page-container">
+      <div className="auth-page-container">
         <div className="auth-left">
-            <img src="src/assets/Screenshot 2025-07-08 at 11.09.00 AM.png" alt="logo-placeholder" className="logo-placeholder"/>
+            <img src="https://images.unsplash.com/photo-1524100880052-104a85d5ab82?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE3fHx8ZW58MHx8fHx8" alt="logo-placeholder" className="logo-placeholder"/>
         </div>
-      <div className="auth-right">
-        <div className="auth-content">
-          <h1>Register</h1>
-          <form>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input
-                type="name"
-                id="name"
-                name="name"
-                placeholder="Enter your name"
-                required
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Enter your email"
-                required
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter your password"
-                required
-              />
-            </div>
-
-            <div className="form-actions">
-              <button
-                type="button"
-                className="cancel-btn"
-                onClick={() => navigate("/")}
-              >
-                Cancel
-              </button>
-              <button type="submit" className="submit-btn">
-                Register
-              </button>
-            </div>
-          </form>
-
-          <div className="login-footer">
-            <a href="http://localhost:5173/login">Already have an account?</a>
+        <div className="auth-right">
+          <div className="w-full h-full flex items-center justify-center">
+            <SignUp/>
           </div>
         </div>
-      </div>
       </div>
     </>
   );

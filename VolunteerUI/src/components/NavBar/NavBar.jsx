42e606bc962f9b06f320f6e9551216101998859a
jandresol <jasmine.andresol@gmail.com>
2025-07-16 14:47:06 -0700

    bug fix

--- VolunteerUI/src/components/NavBar/NavBar.jsx
+++ VolunteerUI/src/components/NavBar/NavBar.jsx
@@ -1,10 +1,13 @@
 import './NavBar.css';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 import { FaUserCircle } from "react-icons/fa";
 import { useUser, useClerk } from '@clerk/clerk-react';
 
 function Navbar() {
     const navigate = useNavigate();
+    const location = useLocation();
+    const isHomePage = location.pathname === "/";
+
     const { signOut } = useClerk();
     const { user } = useUser();
 
@@ -29,24 +32,26 @@ function Navbar() {
             <div className="profile-menu">
                 {isLoggedIn ? (
                     <>
-                        <NavLink to="/profile" className="profile-icon">
-                            {user.publicMetadata.avatarUrl ? (
-                                <img 
-                                    src={user.publicMetadata.avatarUrl} 
-                                    alt={`${user.username || 'User'}'s avatar`} 
-                                    style={{ width: 60, height: 60, borderRadius: '50%' }} 
-                                />
-                            ) : (
-                                <FaUserCircle size={60} />
-                            )}
-                        </NavLink>
-                        <button className="logout-button" onClick={handleLogout}>Logout</button>
+                    <NavLink to="/profile" className="profile-icon">
+                        {user?.publicMetadata?.avatarUrl ? (
+                        <img 
+                            src={user.publicMetadata.avatarUrl} 
+                            alt={`${user.username || 'User'}'s avatar`} 
+                            style={{ width: 60, height: 60, borderRadius: '50%' }} 
+                        />
+                        ) : (
+                        <FaUserCircle size={60} />
+                        )}
+                    </NavLink>
+                    <button className="logout-button" onClick={handleLogout}>Logout</button>
                     </>
                 ) : (
+                    !isHomePage && (
                     <>
                         <NavLink to="/login" className="nav-link">Login</NavLink>
                         <NavLink to="/signup" className="nav-link">Sign Up</NavLink>
                     </>
+                    )
                 )}
             </div>
         </nav>

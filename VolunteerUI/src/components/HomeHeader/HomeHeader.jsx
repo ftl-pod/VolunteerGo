1e49902c14528e6709d8ef76a6aff436423cc075
Jada <jadafinesilver@gmail.com>
2025-07-17 16:17:59 -0700

    cleaning up ui errors

--- VolunteerUI/src/components/HomeHeader/HomeHeader.jsx
+++ VolunteerUI/src/components/HomeHeader/HomeHeader.jsx
@@ -1,8 +1,10 @@
 import "./HomeHeader.css";
 import { FaRegPaperPlane } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
+import { useUser, useClerk} from '@clerk/clerk-react';
 
 function HomeHeader() {
+  const {isSignedIn, isLoaded} = useUser();
   return (
     <div className="home-header">
       <div className="header-left">
@@ -13,16 +15,20 @@ function HomeHeader() {
           <h1>VolunteerGo</h1>
         </div>
       </div>
-      <div className="buttons-for-access header-right">
-        <NavLink to="/login" className="nav-link">
-          <button className="login">LOGIN</button>
-        </NavLink>
-        <NavLink to="/signup" className="nav-link">
-          <button className="register">Register</button>
-        </NavLink>
-      </div>
+      {!isLoaded ? null : isSignedIn ? null : (
+  <div className="buttons-for-access header-right">
+    <NavLink to="/login" className="nav-link">
+      <button className="login">Login</button>
+    </NavLink>
+    <NavLink to="/signup" className="nav-link">
+      <button className="register">Register</button>
+    </NavLink>
+  </div>
+)}
+      
     </div>
-  );
+  
+);
 }
 
 

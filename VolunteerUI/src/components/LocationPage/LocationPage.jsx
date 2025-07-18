e1a86625f7d008c4f13e73a571f548f84c3fde7b
Jada <jadafinesilver@gmail.com>
2025-07-18 11:07:37 -0700

    map marker functionality

--- VolunteerUI/src/components/LocationPage/LocationPage.jsx
+++ VolunteerUI/src/components/LocationPage/LocationPage.jsx
@@ -3,6 +3,9 @@ import React, {use, useEffect, useState} from 'react';
 import {APIProvider, Map, AdvancedMarker} from '@vis.gl/react-google-maps';
 import * as Geocode from 'react-geocode';
 import { useUser } from "@clerk/clerk-react";
+import { useNavigate } from "react-router";
+import { MdLocationOn } from "react-icons/md";
+
 // need to import cluster stuff if i wanna do that
 
 
@@ -14,6 +17,7 @@ function LocationPage () {
     const [userLatLng, setUserLatLng] = useState({ lat: 37.7749, lng: -122.4194 })
     // pulling from publicMetadata -> get user info
     const { user, isLoaded } = useUser();
+    const navigate = useNavigate();
     useEffect(() => {
     const fetchOppsAndCoords = async () => {
         try {
@@ -29,14 +33,17 @@ function LocationPage () {
                 try {
                     const response = await Geocode.fromAddress(o.location);
                     const { lat, lng } = response.results[0].geometry.location;
-                    return { id: o.id, position: { lat, lng } };
+                    return { id: o.id, position: { lat, lng }, title: o.name };
                 } catch (error) {
                     console.error("Error fetching coordinates for", o.location, ":", error);
-                    return { id: o.id, position: { lat: 37.7749, lng: -122.4194 } }; // default to SF if error
+                    // line below is just testing
+                    return { id: o.id, position: { lat: 37.7749, lng: -122.4194 }, title : "Can't find Title" }; // default to SF if error
                 }
             })
         );
+        console.log(coordsArr)
         setCoords(coordsArr);
+        
         } catch (err) {
         console.error("Failed to fetch opportunities:", err);
         }
@@ -75,7 +82,9 @@ function LocationPage () {
                         <AdvancedMarker
                             key={c.id}
                             position={c.position}
-                        >
+                            title={c.title}
+                            onClick={() => navigate(`/opportunity/${c.id}`)}>
+                            <MdLocationOn   className="marker-icon"/>
                         </AdvancedMarker>
                     ))}
                 </Map>
